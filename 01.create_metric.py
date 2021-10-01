829635c1b65874fb62e3b4ca965aa7d246587df5
[USERNAME] <taeyoung.kim@bespinglobal.com>
2021-10-01 03:12:42 +0000

    Test21

--- 01.create_metric.py
+++ 01.create_metric.py
@@ -1,7 +1,7 @@
 from google.cloud import monitoring
 import subprocess
 import sys
-#Test20
+#Test21
 
 
 cmd_id = ['curl','http://metadata.google.internal/computeMetadata/v1/project/project-id', '-H', 'Metadata-Flavor: Google']
