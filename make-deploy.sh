0e7388296508717b2fbe9ab85a7b81a5bde85454
[USERNAME] <taeyoung.kim@bespinglobal.com>
2021-10-06 07:19:56 +0000

    TEST

--- make-deploy.sh
+++ make-deploy.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 echo "Generating deploy.yaml"
+echo "======================"
+cat <<EOF > /deploy/deploy.yaml
+steps:
+- name: 'gcr.io/cloud-builders/gcloud'
+  entrypoint: 'bash'
+  args:
+    - '-c'
+    - |
+      mkdir /builder/home/.ssh
+      ssh-keygen -t rsa -f /builder/home/.ssh/google_compute_engine
+  id: 'init-ssh'
+EOF
+
 while IFS=',' read INSTANCE_NAME ZONE STATUS; 
 do
- cat <<EOF > /deploy/deploy.yaml
-steps:
+ cat <<EOF >> /deploy/deploy.yaml
+
 - name: 'gcr.io/cloud-builders/gcloud'
   args: ['compute', 'ssh', 'pping@${INSTANCE_NAME}', '--zone', '${ZONE}', '--tunnel-through-iap', '--ssh-flag', '-p 10022', '--command', 'hostname']
   id : 'deploy-${INSTANCE_NAME}'
+  waitFor: ['init-ssh']
 EOF
 done < /deploy/list.csv
