5a2327e37766cda3f73cbbc24e12cdb6512102ab
[USERNAME] <taeyoung.kim@bespinglobal.com>
2021-10-06 07:02:15 +0000

    TEST

--- make-deploy.sh
+++ make-deploy.sh
@@ -6,7 +6,6 @@ do
 step:
 - name: 'gcr.io/cloud-builders/gcloud'
   args: ['compute', 'ssh', 'pping@${INSTANCE_NAME}', '--zone', '${ZONE}', '--tunnel-through-iap', '--ssh-flag', '-p 10022', '--command', 'hostname']
-  waitFor: ['init-ssh']
   id : 'deploy-${INSTANCE_NAME}'
 EOF
 done < /deploy/list.csv
