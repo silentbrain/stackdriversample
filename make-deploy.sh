8b561e0483b6d579b6f7a8b85f935b60773774d0
[USERNAME] <taeyoung.kim@bespinglobal.com>
2021-10-06 07:08:28 +0000

    TEST

--- make-deploy.sh
+++ make-deploy.sh
@@ -3,7 +3,7 @@ echo "Generating deploy.yaml"
 while IFS=',' read INSTANCE_NAME ZONE STATUS; 
 do
  cat <<EOF > /deploy/deploy.yaml
-step:
+steps:
 - name: 'gcr.io/cloud-builders/gcloud'
   args: ['compute', 'ssh', 'pping@${INSTANCE_NAME}', '--zone', '${ZONE}', '--tunnel-through-iap', '--ssh-flag', '-p 10022', '--command', 'hostname']
   id : 'deploy-${INSTANCE_NAME}'
