7bba2e7d8328e6c92c8272f7b1c5f6669506007f
[USERNAME] <taeyoung.kim@bespinglobal.com>
2021-10-06 06:19:33 +0000

    TEST

--- make-deploy.sh
+++ make-deploy.sh
@@ -2,11 +2,11 @@
 
 while IFS=',' read INSTANCE_NAME ZONE STATUS; 
 do
- cat <<EOF > /workspace/deploy.yaml
+ cat <<EOF > /deploy/deploy.yaml
 step:
 - name: 'gcr.io/cloud-builders/gcloud'
   args: ['compute', 'ssh', 'pping@${INSTANCE_NAME}', '--zone', '${ZONE}', '--tunnel-through-iap', '--ssh-flag', '-p 10022', '--command', 'hostname']
   waitFor: ['init-ssh']
   id : 'deploy-${INSTANCE_NAME}'
 EOF
-done < /workspace/list.csv
+done < /deploy/list.csv
